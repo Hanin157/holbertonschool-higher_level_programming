608afa759a8e02992c6c12fd2763b410e22b42f6
Hanin157 <alshaneen856@gmail.com>
2026-01-24 16:22:51 +0000

    Fix: Remove len() and handle IndexError

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,20 +4,17 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            if i >= len(my_list):
-                raise IndexError
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
+            # نحاول الوصول إلى العنصر في الفهرس i
+            value = my_list[i]
+            if isinstance(value, int):
+                print("{:d}".format(value), end="")
                 count += 1
-        except ZeroDivisionError:
-            print("division by 0")
-            result.append(0)
-        except TypeError:
-            print("wrong type")
-            result.append(0)
         except IndexError:
             print("Traceback (most recent call last):")
             print("IndexError: list index out of range")
             break
+        except TypeError:
+            print("wrong type")
+            continue
     print()
     return count
