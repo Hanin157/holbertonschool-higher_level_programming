152de43a824a88b2609462324b45f86fb0f98a6f
Hanin157 <alshaneen856@gmail.com>
2026-01-24 16:17:43 +0000

    Fix: Add Traceback for IndexError

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,10 +4,19 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
+            if i >= len(my_list):
+                raise IndexError
             if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
+        except ZeroDivisionError:
+            print("division by 0")
+            result.append(0)
+        except TypeError:
+            print("wrong type")
+            result.append(0)
         except IndexError:
+            print("Traceback (most recent call last):")
             print("IndexError: list index out of range")
             break
     print()
