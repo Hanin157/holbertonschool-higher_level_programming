7087849f61946d73f512d6f76d5ea5d5fcb0ea49
Hanin157 <alshaneen856@gmail.com>
2026-02-02 19:41:50 +0000

    Fix PEP8: shorten docstring in append_write

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,6 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    """Appends a string to a text file and returns the number of characters added."""
+    """Append a string to a file and return number of characters added."""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
