aac654fa24a087e65c31e10b1a78630cd15628ca
Hanin <hanin@example.com>
2026-02-25 23:00:32 +0000

    ORM: filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""List all states from the database hbtn_0e_0_usa that don't start with N."""
+
+import sys
+import MySQLdb
+
+
+def main():
+    """Connect to MySQL and print states not starting with 'N'."""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=dbname,
+        charset="utf8"
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name NOT LIKE 'N%' ORDER BY id ASC;")
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ python-object_relational_mapping/README.md
@@ -0,0 +1,3 @@
+# Python Object Relational Mapping
+
+This project connects Python scripts to MySQL databases using MySQLdb and SQLAlchemy.
